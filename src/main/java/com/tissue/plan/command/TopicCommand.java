b9d33effa6771e96173c2ff82b61f2ba5f0d6f66
guoyingshou <guoyingshou@tianji.com>
2013-03-24 19:53:19 +0800

    bugfix

--- src/main/java/com/tissue/plan/command/TopicCommand.java
+++ src/main/java/com/tissue/plan/command/TopicCommand.java
@@ -1,7 +1,12 @@
 package com.tissue.plan.command;
 
+import com.tissue.core.Account;
 import java.util.Set;
 
-public interface TopicCommand extends PostCommand {
+public interface TopicCommand {
+    String getId();
+    String getTitle();
+    String getContent();
     Set<String> getTags();
+    Account getAccount();
 }
